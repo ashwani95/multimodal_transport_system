88b4db027a5abcadcbc5c1b85fea784c975a9170
vaibhav <vaibhav@holidayiq.com>
2018-08-26 23:43:16 +0530

    public transport changes

--- api/mapbox_api.py
+++ api/mapbox_api.py
@@ -2,18 +2,22 @@ import os
 
 from flask import Flask
 from flask import jsonify
-from config import MAP_BOX_API_KEY
+from api.config import MAP_BOX_API_KEY
 import requests
+import json
 
 app = Flask(__name__)
 
-@app.route('/getRouteInfo')
 def getRouteInfo(sourceLat, sourceLong, destLat, destLong):
-    r = requests.get('https://api.mapbox.com/directions/v5/mapbox/driving-traffic/' + sourceLat + ',' +
-                     sourceLong + ';' + destLat + ',' + destLong + '?access_token='+MAP_BOX_API_KEY+'&steps=true')
-    apiResponse = jsonify(r.json())
-    return apiResponse
-    # return 'Total time: ' + apiResponse.routes[0].duration + ' Total distance: ' + apiResponse.routes[0].distance
+    url = 'https://api.mapbox.com/directions/v5/mapbox/driving-traffic/' + sourceLat + ',' + sourceLong + ';' + destLat + ',' + destLong
+    queryString = {
+        'access_token': MAP_BOX_API_KEY,
+        'steps': 'true'
+    }
+    apiResponse = requests.request('GET', url, params=queryString)
+    jsonResponse = apiResponse.json()
+    result = {"distance": jsonResponse['routes'][0]['distance'], "time": jsonResponse['routes'][0]['duration']}
+    return json.dumps(result)
 
 if __name__ == '__main__':
    app.run()
