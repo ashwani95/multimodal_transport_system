c27867d63e8a315d04efde09c93fa11a70799ca7
vaibhav <vibhav.sachan.vs@gmail.com>
2018-08-28 00:41:36 +0530

    getting all viable route options

--- api/get_public_transport.py
+++ api/get_public_transport.py
@@ -5,14 +5,18 @@ import requests
 import json
 import math
 import sqlite3
+import _mysql
 from mapbox_api import getRouteInfo
 from distance_matrix_api import getDitanceAndTime
 from config import UBER_API_KEY
+
+db = _mysql.connect("localhost", "root", "", "commute")
 
 
 def getFastestRoute(sourceLat, sourceLong, destLat, destLong):
     nearestMetroLocationsToSource, nearestMetroLocationsToDest = getPublicTransport(sourceLat, sourceLong, destLat, destLong)
     totalJourneyTime = math.inf
+    allSourceDestCombos = []
     sourceDestMetroCombo = []
     for sourceMetro in nearestMetroLocationsToSource:
         for destMetro in nearestMetroLocationsToDest:
@@ -27,10 +31,11 @@ def getFastestRoute(sourceLat, sourceLong, destLat, destLong):
             # totalJourneyTime = totalRouteTime
             sourceDestMetroCombo['source'] = sourceMetro
             sourceDestMetroCombo['dest'] = destMetro
-            sourceDestMetroCombo['time'] = totalRouteDetails
-            sourceDestMetroCombo['price'] = totalRouteDetails
+            sourceDestMetroCombo['time'] = totalRouteDetails["totalTime"]
+            sourceDestMetroCombo['price'] = totalRouteDetails["totalPrice"]
+            allSourceDestCombos.append(sourceDestMetroCombo)
 
-    return nearestMetroLocationsToSource, nearestMetroLocationsToDest
+    return allSourceDestCombos
 
 
 def getPublicTransport(sourceLat, sourceLong, destLat, destLong):
@@ -40,52 +45,71 @@ def getPublicTransport(sourceLat, sourceLong, destLat, destLong):
     radiusIncrement = 1
     pi = 22/7
     cityLat = 28.6
-    # input
-    sourceLat = 0
-    sourceLong = 0
-    destLat = 0
-    destLong = 0
-    # get this data from db
-    db = get_db()
-    d.execute(
-        'SELECT * FROM public_coordinates;'
-    ).fetchall()
     metroLocations = []
+    # get this data from db
+    db.query(
+        'SELECT * FROM public_coordinates'
+    )
+    r = db.store_result()
+    metroLocations = r.fetch_row(0, 1)
     nearestMetroLocationsToSource = []
     nearestMetroLocationsToDest = []
 
     # calculate distance of each metro station from source
     # this would be inclusive of auto stands/bus stands, all maybe distinguished by a field in each row
+    sourceMetroFound = False
     while maxDistance < 10:
         for metro in metroLocations:
-            distance = pow((pow((metro.lat-sourceLat)*110.574, 2) + pow((metro.long-sourceLong)*111.320*math.cos(cityLat/180*pi), 2)), 0.5)
+            metroLat = float(metro['coordinates'].decode().split(", ")[0])
+            metroLong = float(metro['coordinates'].decode().split(", ")[1])
+            distance = pow((pow((metroLat-sourceLat)*110.574, 2) + pow((metroLong-sourceLong)*111.320*math.cos(cityLat/180*pi), 2)), 0.5)
             if distance < maxDistance:
-                # metro.distance = distance
+                sourceMetroFound = True
+                metro["distance"] = distance
                 nearestMetroLocationsToSource.append(metro)
+        if sourceMetroFound:
+            break
         maxDistance = maxDistance + radiusIncrement
 
+    print("Nearest Metros :\n")
+    print("To source :\n")
+    print(nearestMetroLocationsToSource)
+
     maxDistance = 1
+    destMetroFound = False
     # repeat for destination
     while maxDistance < 10:
         for metro in metroLocations:
-            distance = pow((pow((metro.lat-destLat)*110.574, 2) + pow((metro.long-destLong)*111.320*math.cos(cityLat/180*pi), 2)), 0.5)
+            metroLat = float(metro['coordinates'].decode().split(", ")[0])
+            metroLong = float(metro['coordinates'].decode().split(", ")[1])
+            distance = pow((pow((metroLat-destLat)*110.574, 2) + pow((metroLong-destLong)*111.320*math.cos(cityLat/180*pi), 2)), 0.5)
             if distance < maxDistance:
-                # metro.distance = distance
+                destMetroFound = True
+                metro["distance"] = distance
                 nearestMetroLocationsToDest.append(metro)
+        if destMetroFound:
+            break
         maxDistance = maxDistance + radiusIncrement
+
+    print("Nearest Metros :\n")
+    print("To dest :\n")
+    print(nearestMetroLocationsToDest)
 
     # time taken if no public transport is considered - straight from source to dest
     # get from API
     directRoute = getRouteInfo(sourceLat, sourceLong, destLat, destLong)
-    directTime = directRoute.routes[0].duration
-    directDistance = directRoute.routes[0].distance
+    directTime = directRoute["time"]
+    directDistance = directRoute["distance"]
 
     for sourceMetro in nearestMetroLocationsToSource:
         routeFromSourceToMetro = getRouteInfo(sourceLat, sourceLong, sourceMetro.latitude, sourceMetro.longitude)
-        timeFromSourceToMetro = routeFromSourceToMetro.routes[0].duration
+        timeFromSourceToMetro = routeFromSourceToMetro["time"]
         # if time from source to metro itself exceeds direct pathing time, remove
         if timeFromSourceToMetro > directTime:
             nearestMetroLocationsToSource.remove(sourceMetro)
+
+    print("After removing\n")
+    print(nearestMetroLocationsToSource)
 
     # similarly for destination
     for destMetro in nearestMetroLocationsToDest:
@@ -98,26 +122,32 @@ def getPublicTransport(sourceLat, sourceLong, destLat, destLong):
     return nearestMetroLocationsToSource, nearestMetroLocationsToDest
 
 
-def get_db():
-    if 'db' not in g:
-        g.db = sqlite3.connect(
-            current_app.config['DATABASE'],
-            detect_types=sqlite3.PARSE_DECLTYPES
-        )
-        g.db.row_factory = sqlite3.Row
-
-    return g.db
-
-
-def close_db(e=None):
-    db = g.pop('db', None)
-
-    if db is not None:
-        db.close()
-
-
 def getTotalJourneyTime(sourceLat, sourceLong, destLat, destLong, sourceMetro, destMetro):
+    sourceMetroLat = sourceMetro["coordinates"].decode().split(", ")[0]
+    sourceMetroLong = sourceMetro["coordinates"].decode().split(", ")[1]
+    destMetroLat = destMetro["coordinates"].decode().split(", ")[0]
+    destMetroLong = destMetro["coordinates"].decode().split(", ")[1]
+
     # Step 1 - get time taken from source to sourceMetro
+    firstLeg = getUberData(sourceLat, sourceLong, sourceMetroLat, sourceMetroLong)
+    firstLegTime = firstLeg['prices'][0]['duration']
+    firstLegPrice = firstLeg['prices'][0]['estimate']
+    print(firstLegTime)
+
+    # Step 2 - get next ETA for bus/metro from that station
+
+    # Step 3 - time taken to travel from sourceMetro to destMetro
+
+    # Step 4 - time taken to travel from destMetro to dest
+    endLeg = getUberData(destMetroLat, destMetroLong, destLat, destLong)
+    endLegTime = endLeg['prices'][0]['duration']
+    endLegPrice = endLeg['prices'][0]['estimate']
+    print(firstLegTime)
+
+    # Step 5 - add all times, including the waiting time
+    return ''
+
+def getUberData(sourceLat, sourceLong, destLat, destLong):
     uberApiURL = "https://api.uber.com/v1.2/estimates/price"
     uberQueryString = {
         "start_latitude": sourceLat,
@@ -130,14 +160,10 @@ def getTotalJourneyTime(sourceLat, sourceLong, destLat, destLong, sourceMetro, d
         'Accept-Language': "en_US",
         'Content-Type': "application/json"
     }
-    firstLeg = requests.request("GET", uberApiURL, headers=uberHeaders, params=uberQueryString)
-    firstLegTime = firstLeg['prices'][0]['duration']
-    firstLegPrice = firstLeg['prices'][0]['estimate']
-    # Step 2 - get next ETA for bus/metro from that station
+    routeData = requests.request("GET", uberApiURL, headers=uberHeaders, params=uberQueryString)
+    routeData = routeData.json()
+    return json.dumps(routeData)
 
-    # Step 3 - time taken to travel from sourceMetro to destMetro
-    # Step 4 - time taken to travel from destMetro to dest
-    # Step 5 - add all times, including the waiting time
-    return ''
 
-# getFastestRoute(28.2, 77.2, 28.5, 77.6)
+# getFastestRoute(28.6, 77.2, 28.5, 77.32)
+getPublicTransport(28.6, 77.2, 28.5, 77.32)
