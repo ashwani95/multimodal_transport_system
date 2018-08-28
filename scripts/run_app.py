e804feb4cd5ba8d945059efb9aefb15c482c7beb
Ashwani <ashwani@deepfence.io>
2018-08-28 16:46:32 +0530

    Enhancing console ui

--- scripts/run_app.py
+++ scripts/run_app.py
@@ -4,7 +4,16 @@ from api.distance_matrix_api import getDistanceAndTime
 
 def start_app():
     create_tranform_set()
+    print("\n-------------------------------------------\n")
     print("\nWelcome to Centralised Commutting System\n")
+
+    booking = True
+    while(booking):
+        status = create_travel_iternary()
+        if not status:
+            booking = False
+
+def create_travel_iternary():
     print("\n-------------------------------------------\n")
     print("\nEnter the below details for find your travel iternary\n")
     print("\nInput Starting Location : (Latitude,Longitude)\n")
@@ -15,11 +24,23 @@ def start_app():
     destLong = float(input("Longitude : "))
 
 
-    distance_matrix_result = json.loads(getDistanceAndTime(sourceLat,sourceLong,destLat,destLong))
-    print("\nJourney Dsitance : "+str(distance_matrix_result["distance_text"]))
-    journey_time = predict_time(float(distance_matrix_result["distance_value"]),float(distance_matrix_result["time_value"]))
-    print("\nEstimated Time Duration :"+journey_time)
 
+    distance_matrix_result = json.loads(getDistanceAndTime(sourceLat, sourceLong, destLat, destLong))
+    print("\n-------------------------------------------\n")
+    print("\nJourney Dsitance : " + str(distance_matrix_result["distance_text"]))
+    journey_time = predict_time(float(distance_matrix_result["distance_value"]),
+                                float(distance_matrix_result["time_value"]))
+    print("\nEstimated Time Duration :" + journey_time)
+
+    print("\n-------------------------------------------\n")
+
+    print("\nPress 1 to continue for another trip. Press 2 to exit\n")
+    choice = int(input("Your Option : "))
+    if choice == 1:
+        return True
+    else:
+        print("\nEnjoy Your Trip :D Bye!!\n")
+        return False
 
 
 start_app()
